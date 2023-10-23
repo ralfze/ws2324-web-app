d9c3dfb851194cc1c1fb1b03e52b836388e1a118
ralfze <razeit01@outlook.com>
2023-10-23 05:04:18 +0200

    Add Springboot Template

---
+++ src/main/java/com/diceweb/diceapp/DiceappApplication.java
@@ -0,0 +1,13 @@
+package com.diceweb.diceapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DiceappApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DiceappApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/diceweb/diceapp/DiceappApplicationTests.java
@@ -0,0 +1,13 @@
+package com.diceweb.diceapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DiceappApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
