7974f562199858e1264a76cbcf2454b299c00ad7
ralfze <razeit01@outlook.com>
2023-11-11 18:03:49 +0100

    Add UI in React

--- src/main/java/com/diceweb/diceapp/DiceController.java
+++ src/main/java/com/diceweb/diceapp/DiceController.java
@@ -1,7 +1,6 @@
 package com.diceweb.diceapp;
 
 import java.util.List;
-import java.util.Random;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
