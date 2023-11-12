63d4627f3877555552cfc149289feae7fd8532ae
ralfze <razeit01@outlook.com>
2023-11-12 01:28:56 +0100

    Add Chakra UI to project

--- diceui/src/App.js
+++ diceui/src/App.js
@@ -1,27 +1,32 @@
 import logo from './logo.svg';
 import './App.css';
+// 1. import `ChakraProvider` component
+import { ChakraProvider } from '@chakra-ui/react'
+
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-        <p>
-        Add React to project
-        </p>
-      </header>
-    </div>
+    <ChakraProvider>
+      <div className="App">
+        <header className="App-header">
+          <img src={logo} className="App-logo" alt="logo" />
+          <p>
+            Edit <code>src/App.js</code> and save to reload.
+          </p>
+          <a
+            className="App-link"
+            href="https://reactjs.org"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            Learn React
+          </a>
+          <p>
+            Add React to project
+          </p>
+        </header>
+      </div>
+    </ChakraProvider>
   );
 }
 
